f2bed84f2dbcdc4f372a6733ea08a665bfa92358
millertim83 <80060826+millertim83@users.noreply.github.com>
2021-09-15 10:45:44 -0400

    addedcomponents

---
+++ src/Components/ExpenseInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExpenseInput() {
+    return (
+
+    )
+}
+
+export default ExpenseInput;
---
+++ src/Components/ExpenseTable.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExpenseTable() {
+    return (
+
+    )
+}
+
+export default ExpenseTable;
