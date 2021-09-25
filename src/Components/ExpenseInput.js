a76b0f61a4471cefc8aa39cd043175f853f41add
millertim83 <80060826+millertim83@users.noreply.github.com>
2021-09-25 12:45:33 -0400

    fixed input submission and placeholder issue

--- src/Components/ExpenseInput.js
+++ src/Components/ExpenseInput.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 const ExpenseInput = ({expenseItems, setExpenseItems}) => {
-    const [date, setDate] = useState(" ");
-    const [amount, setAmount] = useState(" ");
-    const [paymentMethod, setPaymentMethod] = useState(" ");
-    const [description, setDescription] = useState(" ");
-    const [purchaseLocation, setPurchaseLocation] = useState(" ");
+    const [date, setDate] = useState("");
+    const [amount, setAmount] = useState("");
+    const [paymentMethod, setPaymentMethod] = useState("Cash");
+    const [description, setDescription] = useState("");
+    const [purchaseLocation, setPurchaseLocation] = useState("");
 
     const handleExpenseSubmit = (e) => {
         e.preventDefault();
@@ -52,7 +52,7 @@ const ExpenseInput = ({expenseItems, setExpenseItems}) => {
                     <div className="col">
                         <label className="form-label">Price: </label>
                         <input 
-                            value={amount}
+                            value={amount} 
                             required
                             type="number" 
                             placeholder="How much?" 
@@ -65,12 +65,12 @@ const ExpenseInput = ({expenseItems, setExpenseItems}) => {
                     <div className="col">
                         <label className="form-label">Payment method: </label>
                             <select 
-                                value={paymentMethod}
+                                value={paymentMethod} 
+                                required
                                 className="col form-select-sm border-2 rounded ms-2" 
                                 name="payment-method" id="payment-method"
                                 onChange={(e) => setPaymentMethod(e.target.value)}
                                 >
-                                <option value=""></option>;
                                 <option value="cash">Cash</option>
                                 <option value="debit">Debit</option>
                                 <option value="credit">Credit</option>
@@ -81,12 +81,12 @@ const ExpenseInput = ({expenseItems, setExpenseItems}) => {
                     </div>
                 </div>
                 
-                <div className="row mt-3">
+                <div className="row">
                     <div className="col">
                         <label className="form-label">Expense description: </label>
                         <input 
-                            value={description}
                             required
+                            value={description} 
                             type="text" 
                             placeholder="What did you buy?" 
                             id="expenseDescription" 
